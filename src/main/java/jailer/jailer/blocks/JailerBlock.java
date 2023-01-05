62302eb87cb025a0cecf954138207556818b5279
MrEnxo <somewhatenzo@gmail.com>
2023-01-05 17:51:27 -0600

    mining yes

--- src/main/java/jailer/jailer/blocks/JailerBlock.java
+++ src/main/java/jailer/jailer/blocks/JailerBlock.java
@@ -1,21 +1,25 @@
 package jailer.jailer.blocks;
 
 import org.bukkit.Material;
+import static org.bukkit.Sound.*;
+import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.inventory.ItemStack;
 
 public enum JailerBlock {
     Stone(new LootTable<>(
             new LootTable.Entry<>(BlockDrop.Stone, 1)
-    ), 30),
-    Unbreakable(new LootTable(), 100000000);
+    ), BLOCK_STONE_BREAK,30),
+    Unbreakable(new LootTable(), BLOCK_STONE_BREAK,100000000);
 
     public final int durability;
+    public final Sound sound;
     public final LootTable<BlockDrop> lootTable;
 
-    JailerBlock(LootTable<BlockDrop> lootTable, int durability) {
+    JailerBlock(LootTable<BlockDrop> lootTable, Sound sound, int durability) {
         this.lootTable = lootTable;
         this.durability = durability;
+        this.sound = sound;
     }
 
 
